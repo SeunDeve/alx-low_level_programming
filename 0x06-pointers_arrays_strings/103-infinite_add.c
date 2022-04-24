3cb4f353c94ba46b9fecc192944978664602635a
SeunDeve <eseun.oyewole@gmail.com>
2022-04-24 12:46:31 +0000

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -3,9 +3,8 @@
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
-
 /**
- *add_strings - Adds the numbers stored in two strings.
+ * add_strings - Adds the numbers stored in two strings.
  * @n1: The string containing the first number to be added.
  * @n2: The string containing the second number to be added.
  * @r: The buffer to store the result.
@@ -18,7 +17,6 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
 	int num, tens = 0;
 
-
 	for (; *n1 && *n2; n1--, n2--, r_index--)
 	{
 		num = (*n1 - '0') + (*n2 - '0');
@@ -29,20 +27,32 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 
 	for (; *n1; n1--, r_index--)
 	{
+		num = (*n1 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n2; n2--, r_index--)
+	{
 		num = (*n2 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	if (tens && r_index >= 0)
+	{
+		*(r + r_index) = (tens % 10) + '0';
 		return (r + r_index);
 	}
-	
+
 	else if (tens && r_index < 0)
 		return (0);
 
 	return (r + r_index + 1);
-
 }
 /**
  * infinite_add - Adds two numbers.
- * @n1: The first number o be added.
+ * @n1: The first number to be added.
  * @n2: The second number to be added.
  * @r: The buffer to store the result.
  * @size_r: The buffer size.
@@ -50,16 +60,18 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
  * Return: If r can store the sum - a pointer to the result.
  * If r cannot store the sum - 0.
  */
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r)	
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int index, n1_len = 0, n2_len = 0;
 
-	for (index = 0; *(n1 + index); indexx++)
+	for (index = 0; *(n1 + index); index++)
+		n1_len++;
+
+	for (index = 0; *(n2 + index); index++)
 		n2_len++;
 
 	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
-		retutn (0);
+		return (0);
 
 	n1 += n1_len - 1;
 	n2 += n2_len - 1;
@@ -67,7 +79,3 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	return (add_strings(n1, n2, r, --size_r));
 }	
-
-	   	
-
-	
